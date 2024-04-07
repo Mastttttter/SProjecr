a8eaaf52fc6dea6e5fe566ae6a6b9dd3a7258df4
Master <cucuohohcooo@gmail.com>
2024-04-07 23:26:41 +0800

    Initial Project

---
+++ SProjrctBackend/src/main/java/org/master/sprojrctbackend/SProjrctBackendApplication.java
@@ -0,0 +1,13 @@
+package org.master.sprojrctbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SProjrctBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SProjrctBackendApplication.class, args);
+    }
+
+}
---
+++ SProjrctBackend/src/test/java/org/master/sprojrctbackend/SProjrctBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package org.master.sprojrctbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SProjrctBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
